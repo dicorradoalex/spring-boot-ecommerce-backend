6246189ea92f42973205b2d6cf4640078be2355c
Alex Di Corrado <dicorradoalex@gmail.com>
2026-04-03 15:59:49 +0200

    feat: add CategoryMapper to codebase

--- src/main/java/com/lipari/Academy2026/mapper/CategoryMapper.java
+++ src/main/java/com/lipari/Academy2026/mapper/CategoryMapper.java
@@ -1,18 +1,19 @@
 package com.lipari.Academy2026.mapper;
 
-import com.lipari.Academy2026.dto.ProductDTO;
-import com.lipari.Academy2026.entity.ProductEntity;
+import com.lipari.Academy2026.dto.CategoryDTO;
+import com.lipari.Academy2026.entity.CategoryEntity;
 import org.mapstruct.Mapper;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring")
-public interface ProductMapper {
+@Mapper(componentModel = "spring") // Permette a Spring di gestire il Mapper come un suo componente (Bean). Questo permette di poterlo utilizzare da altre parti senza fare new CategoryMapperImpl()
+public interface CategoryMapper {
 
-    ProductDTO toDto(ProductEntity pEntity);
+    CategoryDTO toDto(CategoryEntity pEntity);
 
-    ProductEntity toEntity(ProductDTO pDto);
+    CategoryEntity toEntity(CategoryDTO pDto);
 
-    List<ProductDTO> toDtoList(List<ProductEntity> pEntities);
+    List<CategoryDTO> toDtoList(List<CategoryEntity> entityList);
 
+    List<CategoryEntity> toEntityList(List<CategoryDTO> dtoList);
 }
