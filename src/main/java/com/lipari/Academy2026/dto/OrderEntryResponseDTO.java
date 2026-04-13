ca741e3646c14f0caeb2c48056270839c72b3d04
Alex Di Corrado <dicorradoalex@gmail.com>
2026-04-13 18:14:32 +0200

    refactor(OrderEntryResponseDTO): remove comments

--- src/main/java/com/lipari/Academy2026/dto/OrderEntryResponseDTO.java
+++ src/main/java/com/lipari/Academy2026/dto/OrderEntryResponseDTO.java
@@ -18,14 +18,6 @@ public record OrderEntryResponseDTO(
     - record
       Classe immutabile per trasportare i dati della singola riga di un ordine.
 
-    Annotazioni Validation
-    - @Positive
-      Impedisce l'inserimento di valori negativi o zero.
-
-    - @NotNull
-      Controlla solo che l'oggetto non sia null.
-      Accetta stringhe vuote "" o spazi " ". Si usa per numeri e oggetti.
-
     OMISSIONE DI OrderResponseDTO order
 
     - Se OrderResponseDTO contiene una List<OrderEntryResponseDTO> e OrderEntryResponseDTO contiene
