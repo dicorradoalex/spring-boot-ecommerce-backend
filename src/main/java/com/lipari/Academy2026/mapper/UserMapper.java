8651fd77bc8b2e87651a0777ae7898d8500e6b03
Alex Di Corrado <dicorradoalex@gmail.com>
2026-04-16 15:58:12 +0200

    feat(UserMapper): configure the mapper for security

--- src/main/java/com/lipari/Academy2026/mapper/UserMapper.java
+++ src/main/java/com/lipari/Academy2026/mapper/UserMapper.java
@@ -1,5 +1,6 @@
 package com.lipari.Academy2026.mapper;
 
+import com.lipari.Academy2026.dto.UserRegistrationRequestDTO;
 import com.lipari.Academy2026.dto.UserResponseDTO;
 import com.lipari.Academy2026.entity.UserEntity;
 import org.mapstruct.Mapper;
@@ -11,12 +12,12 @@ import java.util.List;
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
-    @Mapping(target = "orders", ignore = true)
+    // Da richiesta di registrazione a Entity (per salvare nel DB)
+    UserEntity toEntity(UserRegistrationRequestDTO registrationDTO);
+
+    // Da Entity a risposta (per inviare al client senza password)
     UserResponseDTO toDto(UserEntity entity);
-    UserEntity toEntity(UserResponseDTO dto);
-    List<UserResponseDTO> toDtoList(List<UserEntity> entityList);
 
-    @Mapping(target = "id", ignore = true)
-    void updateEntityFromDto(UserResponseDTO dto, @MappingTarget UserEntity entity);
-
+    // Da Lista di entity a lista di DTO
+    List<UserResponseDTO> toDtoList(List<UserEntity> entities);
 }
