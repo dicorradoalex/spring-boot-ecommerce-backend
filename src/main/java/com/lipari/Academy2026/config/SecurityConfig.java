d78c0c7bece548f18646abc96939160b4ef8d966
Alex Di Corrado <dicorradoalex@gmail.com>
2026-04-18 14:45:13 +0200

    feat: refactor user management and implement role-based controllers

--- src/main/java/com/lipari/Academy2026/config/SecurityConfig.java
+++ src/main/java/com/lipari/Academy2026/config/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
     private final UserDetailsService userDetailsService;
 
     /**
-     * Definisco il sistema di criptazione delle password (Hashing).
+     * Sistema di criptazione delle password (Hashing).
      * Usiamo BCrypt.
      */
     @Bean
@@ -81,6 +81,9 @@ public class SecurityConfig {
                         .requestMatchers("/api/product/**").permitAll()
                         .requestMatchers("/api/category/**").permitAll()
                         .requestMatchers("/error").permitAll()
+
+                        // Area Amministrativa: accessibile solo con ruolo ADMIN
+                        .requestMatchers("/api/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
 
